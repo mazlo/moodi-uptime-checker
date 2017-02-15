1044ecab01c32b30e2641f41809308497bfd748f
Matthäus Zloch <matthaeus.zloch@gmail.com>
2017-02-15 10:55:51 +0100

    app.js, fix -> use extend json parsing

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(logger(':method :url :status :response-time ms, :remote-addr',
 }));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
