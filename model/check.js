db222f86a4a41732596f658b7912a3350d793dee
Matthäus Zloch <matthaeus.zloch@gmail.com>
2017-02-18 00:31:57 +0100

    config, save -> after initiation of check iterate assumptions and save results in check

--- model/check.js
+++ model/check.js
@@ -1,11 +1,18 @@
 var mongoose = require( 'mongoose' ),
     Schema = mongoose.Schema
 
+var assumptionExecutedSchema = new Schema({
+    assumption: { type: Schema.ObjectId, ref: 'Assumption' },
+    succeed: Boolean,
+    value_returned: String
+})
+
 var checkSchema = new Schema({
     when: Number,
     duration: Number,
     response: String,
-    statusCode: Number
+    statusCode: Number,
+    assumptions: [assumptionExecutedSchema]
 })
 
 var assumptionSchema = new Schema({
@@ -33,4 +40,5 @@ var checkConfigSchema = new Schema({
 
 mongoose.model( 'Check', checkSchema );
 mongoose.model( 'Assumption', assumptionSchema );
+mongoose.model( 'AssumptionExecuted', assumptionExecutedSchema );
 mongoose.model( 'CheckConfig', checkConfigSchema );
